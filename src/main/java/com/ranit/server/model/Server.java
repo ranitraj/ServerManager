9a6555233bc4d54fcd6a58bb6e8e000034ea7a5c
ranitrajganguly@gmail.com <ranitrajganguly@gmail.com>
2023-02-07 08:51:29 -0700

    Adding decorators in the Server model class

--- src/main/java/com/ranit/server/model/Server.java
+++ src/main/java/com/ranit/server/model/Server.java
@@ -1,9 +1,23 @@
 package com.ranit.server.model;
 
 import com.ranit.server.enumeration.Status;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Server {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @Column(unique = true)
+    @NotEmpty(message = "IP Address cannot be empty")
     private String ipAddress;
     private String name;
     private String memory;
