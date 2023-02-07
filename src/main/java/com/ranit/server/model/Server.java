341482cf87cd893270d090735af15f90d063e90e
ranitrajganguly@gmail.com <ranitrajganguly@gmail.com>
2023-02-06 23:57:42 -0700

    Adding Server model and Status enum classes

---
+++ src/main/java/com/ranit/server/model/Server.java
@@ -0,0 +1,13 @@
+package com.ranit.server.model;
+
+import com.ranit.server.enumeration.Status;
+
+public class Server {
+    private Long id;
+    private String ipAddress;
+    private String name;
+    private String memory;
+    private String type;
+    private String imageUrl;
+    private Status status;
+}
